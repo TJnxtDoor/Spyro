50ba7b84ba1a56e4b94534c29f9797c6e4832f8e
TJ <afzaltj81@gmail.com>
2025-07-20 16:28:33 -0400

    updated world 1

--- Worlds/World 1/wolrd1_placeholder.cs
+++ Worlds/World 1/wolrd1_placeholder.cs
@@ -1,24 +1,79 @@
-UnityEngineUI;
-UnityEngine;
-WorldUI;
+using UnityEngine;
+using UnityEngine.UI;
 
-namespace World1 {
+namespace World1
+{
     public class World1 : MonoBehaviour
     {
-         public GameObject[] worldTrees;
+        public GameObject[] worldTrees;
         public float maxTreeScale = 1.5f;
         public float minTreeScale = 0.5f;
         public float scaleResponseSpeed = 2f;
         public float scaleResponseProgression = 0f;
-        private float currentProgression  = 0f;
+        private readonly float currentProgression = 0f;
         public float playerHealth = 100f;
-        public float playerLives = 5f
+        public float playerLives = 5f;
+        public Text healthText;
+        public float enemyDamage = 10f;
+        public float enemyHealth = 50f;
+
+        void Update()
+        {
+            updatePlayerHealth();
+            Mathf.Clamp(playerHealth, 0, 100);
+        }
+
+        void updatePlayerHealth()
+        {
+            if (playerHealth <= 0)
+            {
+                playerLives -= 1;
+                playerHealth = 100f; // reset players heath after death
+                if (playerLives <= 0)
+                {
+                    Debug.Log("Player has died");
+
+                }
+
+            }
+        }
+        private void ChangePlayerHealth(float amount)
+        {
+            playerHealth += amount;
+            playerHealth = Mathf.Clamp(playerHealth, 0, 100);
+        }
+
+
+        // enemy damage and health
+        private void HandleEnemyDamage()
+        {
+            playerHealth -= enemyDamage;
+            playerHealth = Mathf.Clamp(playerHealth, 0, 100);
+        }
+
+        private void HandleEnemyDeath()
+        {
+            enemyHealth -= playerDamage;
+            enemyHealth = Mathf.Clamp(enemyHealth, 0, 100);
+        }
+
+        private void OnApplicationQuit(KEYBOARD_EVENT Q)
+        {
+            Debug.Log("Quit Game!");
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Debug.Log("Yes");
+            }
+            else if (Input.GetKeyDown(KeyCode.B))
+            {
+                Debug.Log("No");
+            }
+        }
+       
+
+        private void OnDestroy()
+        {
+            Debug.Log("Game Save Successful");
+        }
     }
-
-
-    void Update()
-
-{
-    updatePlayerHeath()
-}
 }
