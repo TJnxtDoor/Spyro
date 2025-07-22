3d9f318015c334cc63db04262ff1685df49f75cb
TJ <afzaltj81@gmail.com>
2025-07-22 18:27:41 -0400

    Update wolrd1_placeholder.cs

--- Worlds/World 1/wolrd1_placeholder.cs
+++ Worlds/World 1/wolrd1_placeholder.cs
@@ -1,24 +1,20 @@
-UnityEngineUI;
-UnityEngine;
-WorldUI;
+using UnityEngine.UI;
+using UnityEngine;
+using WorldUI;
 
 namespace World1 {
     public class World1 : MonoBehaviour
     {
-         public GameObject[] worldTrees;
+        public GameObject[] worldTrees;
         public float maxTreeScale = 1.5f;
         public float minTreeScale = 0.5f;
         public float scaleResponseSpeed = 2f;
         public float scaleResponseProgression = 0f;
-        private float currentProgression  = 0f;
+        private float currentProgression = 0f;
         public float playerHealth = 100f;
-        public float playerLives = 5f
+        public float playerLives = 5f;
     }
 
-
-    void Update()
-
-{
-    updatePlayerHeath()
 }
-}
+
+
